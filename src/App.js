7e9851f037ab8d948b7c482b1fa6a714a1c90f1e
Deepika <thotadeepika5757@gmail.com>
2024-09-08 14:26:22 +0530

    Forecast

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { CiSearch } from "react-icons/ci";
+import './App.css'
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const apiKey = 'ae55d65cc46bd7d4bb624c58d16547e4';
+
+  const handleSearch = async () => {
+    if (!city) {
+      setError('Please enter a city name.');
+      return;
+    }
+    
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      setWeather(response.data);
+      setError('');
+      setCity('')
+
+    } catch (err) {
+      setError('City not found. Please try again.');
+      setWeather(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className='main-heading'>Weather Forecast</h1>
+      <div className="search-bar">
+        <input 
+          type="text" 
+          className='user-input'
+          value={city} 
+          onChange={(e) => setCity(e.target.value)} 
+          placeholder="Enter city name" 
+        />
+      <button className='button' onClick={handleSearch}><CiSearch/></button>
+      </div>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      {weather && (
+        <div className="weather-info">
+          <h2 className='heading'>Weather in {weather.name}</h2>
+          <p className='weather'>Temperature: {weather.main.temp}°C</p>
+          <p className='weather'>Weather: {weather.weather[0].description}</p>
+          <p className='weather'>Humidity: {weather.main.humidity}%</p>
+          <p className='weather'>Wind Speed: {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
